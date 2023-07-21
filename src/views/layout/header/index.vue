4c706f5cfd39f16357f3774b77c11467423776d0
清梦徐徐丶莫 <97624915@qq.com>
2023-07-21 22:45:31 +0800

    feat: 更新ui

--- src/views/layout/header/index.vue
+++ src/views/layout/header/index.vue
@@ -8,15 +8,39 @@
       <Expand v-if="system.menuStatus" @click="changeMenuStatus()" />
       <Fold v-else @click="changeMenuStatus()" />
     </el-icon>
+    <div class="mz-header-right">
+      <div class="avatar-icon">
+        <el-avatar>
+          <img src="./imgs/avatar.png" alt="">
+        </el-avatar>
+      </div>
+      <svg-icon @click="exit" class="exit-icon" name="tuichu"></svg-icon>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ElMessage, ElMessageBox } from 'element-plus'
+import type { Action } from 'element-plus'
 import { useSystemStore } from '@/stores/system'
 const system = useSystemStore()
 
 const changeMenuStatus = () => {
   system.changeMenuStatus(!system.menuStatus)
+}
+
+// 退出
+const exit = () => {
+  ElMessageBox.alert('是否退出登陆?', '提示', {
+    autofocus: false,
+    confirmButtonText: '确认',
+    callback: (action: Action) => {
+      ElMessage({
+        type: 'info',
+        message: `action: ${action}`
+      })
+    }
+  })
 }
 </script>
 
@@ -24,10 +48,12 @@ const changeMenuStatus = () => {
 .mz-header {
   width: 100%;
   height: 60px;
+  padding: 0 16px;
   background: #fff;
   border-bottom: 1px solid #ccc;
   display: flex;
   align-items: center;
+  justify-content: space-between;
 
   &-logo {
     width: 200px;
@@ -51,9 +77,27 @@ const changeMenuStatus = () => {
   }
 
   .menu-icon {
-    margin-left: 14px;
     font-size: 20px;
     cursor: pointer;
+  }
+
+  &-right {
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    .avatar-icon {
+      width: 40px;
+      height: 40px;
+      margin-right: 16px;
+      img {
+        width: 100%;
+        height: 100%;
+      }
+    }
+    .exit-icon {
+      font-size: 26px;
+      cursor: pointer;
+    }
   }
 }
 </style>
